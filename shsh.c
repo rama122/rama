4b306920cb6a88386790a86cafde085b66768caf
rama122 <47264172+rama122@users.noreply.github.com>
2019-02-06 21:59:31 +0530

    Create shsh.c

---
+++ shsh.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    char n;
+    scanf("%c",&n);
+    if((n=='a')||(n=='A')||(n=='e')||(n=='E')||(n=='i')||(n=='I')||(n=='o')||(n=='O')||(n=='u')||(n=='U'))
+    printf("vowal");
+    else
+    printf("consonant");
+}
